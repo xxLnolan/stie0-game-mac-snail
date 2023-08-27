0ea34eb5b78f032a58e92f54d7ed214ae41d39dc
xxLnolan <laveniernolan@gmail.com>
2023-08-27 16:05:36 +0200

    Add design file

---
+++ the-mast-d/README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:51583787c799c2dd82a0b3c5b1de4b83b78d24c797587c41b21327ee66568c62
+size 154
---
+++ the-mast-d/resources/app/electron-main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0127818e76b47234f09dd18ec05c2a01570a737fa432631901d00d4931f099e
+size 4259
